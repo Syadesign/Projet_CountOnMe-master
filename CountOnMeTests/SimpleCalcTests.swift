a89466892d3e2cd59e93a47085cb686ebd4aeac1
Samahir <samahiradi@gmail.com>
2019-06-11 16:38:26 +0200

    Begin unit Test

--- CountOnMeTests/SimpleCalcTests.swift
+++ CountOnMeTests/SimpleCalcTests.swift
@@ -8,26 +8,20 @@
 
 import XCTest
 
+@testable import CountOnMe
+
 class SimpleCalcTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func test_Given() {
+    
+        let operation = Operations()
+        var numbersArray = operation.numbersArray
+        numbersArray[2] = "+"
+        XCTAssertFalse(operation.expressionIsCorrect)
+        
         }
-    }
-
 }
